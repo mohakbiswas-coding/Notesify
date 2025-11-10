f6f153ead93fbc12443716b371206e5dfc61fbeb
Mohak Biswas <124465889+mohakbiswas-coding@users.noreply.github.com>
2025-11-11 02:53:16 +0530

    04: Backend Done and Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import toast from 'react-hot-toast';
+
+const App = () => {
+  return (
+    <div>
+      <button className='btn btn-primary'>
+        click me
+      </button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
