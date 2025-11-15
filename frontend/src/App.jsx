34e7280fb2b70abe4ec1f085bfffd6c17880493a
Mohak Biswas <124465889+mohakbiswas-coding@users.noreply.github.com>
2025-11-15 15:47:17 +0530

    Copied Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,19 @@
-import { Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
-import CreatePage from './pages/CreatePage';
-import NoteDetailPage from './pages/NoteDetailPage';
-import toast from 'react-hot-toast';
+import { Route, Routes } from "react-router";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
-    <div>
-      <button className='btn btn-primary'>
-        click me
-      </button>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
-}
-
+  );
+};
 export default App;
